c95e654d3b379a1735e8dbe43f5dfaf28dbd4f0d
moaad99 <moadlzrak@gmail.com>
2021-10-20 16:26:11 +0100

    DesigPattern Composite

---
+++ Desktop/DesignPattern-Composite/src/Composite/Folder.java
@@ -0,0 +1,32 @@
+package Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Folder extends Composant {
+	
+	private List<Composant> childs= new ArrayList<>();
+
+	public Folder(String name) {
+		super(name);
+	}
+	
+	public Composant addComponent(Composant comp) {
+		comp.level=this.level+1;
+		childs.add(comp);
+		return comp;	
+	}
+	@Override
+	public void show() {
+	       System.out.println(tab()+"folder : "+name);
+	       
+	       for(Composant c:childs) {
+	    	   c.show();
+	       }
+	       
+
+		
+     		
+	}
+
+}
